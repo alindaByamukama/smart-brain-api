30ce98710fc6d390e51cbc9d5d8f5768362ac9f4
Alinda Byamukama <alindabyamukama@gmail.com>
2024-07-05 20:10:53 +0300

    add api key env var

--- controllers/image.js
+++ controllers/image.js
@@ -2,7 +2,7 @@ const Clarifai = require('clarifai')
 
 // You must add your own API key here from Clarifai
 const app = new Clarifai.App({
-    apiKey: 'fc4c8606d6764cc19fb5b8455675353b'
+    apiKey: 'CLARIFAI_API_KEY'
 })
 
 const handleApiCall = (req, res) =>  {
